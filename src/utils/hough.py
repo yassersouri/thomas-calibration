50733fddc735713c4dc1eaac079387f56832b48c
Yasser Souri <yassersouri@gmail.com>
2012-12-28 13:49:30 +0330

    remove unnecessary and not visible range from theta

--- src/utils/hough.py
+++ src/utils/hough.py
@@ -7,19 +7,19 @@ def general_hough_line(line_image):
 	parameters:
 		-line_image: numpy array, dtype: numpy.float32. This is the output of `line_filter.line_detection_filter(img, keyer)`
 	returns:
-		-numpy array, dtype: numpy.float32, shape: (360,int(sqrt(line_image.shape[0]**2 + line_image.shape[1]**2)))
+		-numpy array, dtype: numpy.float32, shape: (270,int(sqrt(line_image.shape[0]**2 + line_image.shape[1]**2)))
 	"""
 	r_max = int(math.sqrt(line_image.shape[0]**2 + line_image.shape[1]**2))
-	result = numpy.zeros((360, r_max), dtype=numpy.float32)
+	result = numpy.zeros((270, r_max), dtype=numpy.float32)
 
 	for x in range(line_image.shape[0]):
 		for y in range(line_image.shape[1]):
 			value = line_image[x,y]
 			impact_value = value/10000.0
 			if value > 0:
-				for theta in range(0, 360):
+				for theta in range(0, 270):
 					# the real theta
-					r_theta = math.radians(theta - 180)
+					r_theta = math.radians(theta - 90)
 					r = int(y*math.cos(r_theta) + x*math.sin(r_theta))
 					result[theta, r] += impact_value
 
